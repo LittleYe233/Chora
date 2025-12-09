81643e5f8b94cc4d292959b31e474b5c19c4a58c
Zhifan Ye <littleye233@gmail.com>
2025-12-09 22:34:59 +0800

    build: merge pr-issue-43-songlist-buttons

--- app/src/main/java/com/craftworks/music/ui/screens/SongsScreen.kt
+++ app/src/main/java/com/craftworks/music/ui/screens/SongsScreen.kt
@@ -1,11 +1,23 @@
 package com.craftworks.music.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.PlayArrow
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
@@ -14,6 +26,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -21,8 +34,10 @@ import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.media3.common.util.Log
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.session.MediaController
 import com.craftworks.music.R
@@ -87,22 +102,88 @@ fun SongsScreen(
                 }
             },
         ) { innerPadding ->
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = innerPadding.calculateTopPadding())
+            Column(
+                modifier = Modifier.fillMaxSize()
+                    .padding(innerPadding)
             ) {
-                SongsHorizontalColumn(
-                    songList = allSongsList,
-                    onSongSelected = { songs, index ->
-                        println("Starting song at index: $index")
-                        coroutineScope.launch {
-                            SongHelper.play(songs, index, mediaController)
+                // Buttons
+                Row(
+                    modifier = Modifier.fillMaxWidth()
+                        .height(64.dp),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    // Play all
+                    Button(
+                        onClick = {
+                            Log.d("SongsScreenPlayAll",
+                                "Starting song at first index: 0. Song list size: ${allSongsList.size}")
+                            coroutineScope.launch {
+                                SongHelper.play(
+                                    allSongsList,
+                                    0,
+                                    mediaController
+                                )
+                            }
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                            contentColor = MaterialTheme.colorScheme.onBackground),
+                        modifier = Modifier
+                            .widthIn(min = 128.dp, max = 320.dp)
+                    ) {
+                        Row (verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.height(24.dp)
+                        ) {
+                            Icon(Icons.Rounded.PlayArrow, "Play Songs")
+                            Text(stringResource(R.string.Action_Play), maxLines = 1)
                         }
-                    },
-                    isSearch = false,
-                    viewModel = viewModel
-                )
+                    }
+                    // Shuffle all
+                    Button(
+                        onClick = {
+                            mediaController?.shuffleModeEnabled = true
+                            val random = allSongsList.subList(1, allSongsList.size).indices.random()
+                            Log.d("SongsScreenShuffleAll",
+                                "Starting song at random index: $random. Song list size: ${allSongsList.size}")
+                            coroutineScope.launch {
+                                SongHelper.play(
+                                    allSongsList.subList(1, allSongsList.size),
+                                    random,
+                                    mediaController
+                                )
+                            }
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                            contentColor = MaterialTheme.colorScheme.onBackground),
+                        modifier = Modifier
+                            .widthIn(min = 128.dp, max = 320.dp)
+                    ) {
+                        Row (verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.height(24.dp)
+                        ) {
+                            Icon(ImageVector.vectorResource(R.drawable.round_shuffle_28), "Shuffle Songs")
+                            Text(stringResource(R.string.Action_Shuffle), maxLines = 1)
+                        }
+                    }
+                }
+                // List
+                Box(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    SongsHorizontalColumn(
+                        songList = allSongsList,
+                        onSongSelected = { songs, index ->
+                            println("Starting song at index: $index")
+                            coroutineScope.launch {
+                                SongHelper.play(songs, index, mediaController)
+                            }
+                        },
+                        isSearch = false,
+                        viewModel = viewModel
+                    )
+                }
             }
         }
     }
